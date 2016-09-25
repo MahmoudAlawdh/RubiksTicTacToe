540827427f2040685394e01aed95a2bb35f44a61
MahmoudAlawadh <mhd-10@hotmail.com>
2016-09-25 18:34:13 +0300

    optimization

--- TicTacToe.pde
+++ TicTacToe.pde
@@ -2,38 +2,44 @@ import peasy.test.*;
 import peasy.org.apache.commons.math.*;
 import peasy.*;
 import peasy.org.apache.commons.math.geometry.*;
-bigcube rubiks;
 
+bigcube rubiks;
+button b;
 PGraphicsOpenGL pg;
 PeasyCam pcam;
-int turn = 1;
-int time;
-int p1win = 0;
-int p2win = 0;
-int textc = 100;
-void setup() {
+int turn = 1, time,p1win = 0, p2win =0, texts = 100;
+
+void setup()
+{
   size(640, 480,P3D);
   cursor(CROSS);
   pg = (PGraphicsOpenGL)createGraphics(width, height, OPENGL);
   pcam = new PeasyCam(this, 150);
   rubiks = new bigcube();
+  b = new button(width-60, 10, 50, 30, color(255), width-60, 30, "reset");
 }
-
-void draw() {
+void t(color c, String s,int w,int h)
+{
+  textSize(20);
+  fill(c);
+  text(s,w,h);
+}
+void draw()
+{
   background(0,0,0);
-  pointLight(255,255,255,pcam.getPosition()[0],pcam.getPosition()[1],pcam.getPosition()[2]);
+  pointLight(255, 255, 255, pcam.getPosition()[0], pcam.getPosition()[1], pcam.getPosition()[2]);
   rubiks.display();
   pcam.beginHUD();
+  b.draw(mouseX,mouseY);
   textSize(20);
-  reset();
-  fill(255, 0, 0);
-  text("Player1 : "+p1win,0,height-10);
-  fill(0,0,255);
-  text("Player2 : "+p2win,width-110,height-10);
   if(turn == 1 || turn == 2)
-    time = (millis()-rubiks.start)/1000;
-  fill(255, 255, 255);
-  text("Time :"+time, width/30, 30);
+  {
+    time = (millis() - rubiks.start)/1000;
+  }
+  t(color(255,0,0),"Player1 : " + p1win ,0,height-10);
+  t(color(0,0,255),"Player2 : " + p2win,width-110 , height-10);
+  t(color(255,255,255),"Time :"+time, width/30, 30);
+
   if(turn == 1)
   {
     fill(255, 0, 0);
@@ -54,13 +60,14 @@ void draw() {
     fill(0,0,255);
     text("Player 2 wins" , width/2 - width/10, 30);
   }
-
   pcam.endHUD();
+
   }
 
 
 Cube pickCube(int x, int y) {
   Cube cube = null;
+
   pg.projection.set(((PGraphicsOpenGL)g).projection);
   pg.camera.set(((PGraphicsOpenGL)g).camera);
   pg.beginDraw();
@@ -71,6 +78,7 @@ Cube pickCube(int x, int y) {
   for (int i = 0; i < rubiks.cubes.length; i++)
     rubiks.cubes[i].displayForPicker();
   int c = pg.get(x, y);
+
   pg.endDraw();
   for (int i = 0; i < rubiks.cubes.length; i++) {
     if (rubiks.cubes[i].pickCol == c) {
@@ -81,55 +89,48 @@ Cube pickCube(int x, int y) {
   return cube;
 }
 
-
 void mouseClicked  () {
   if (mouseButton == RIGHT)
   {
     Cube picked = pickCube(mouseX, mouseY);
-      if (picked != null) {
-        if(turn == 1)
+    if (picked != null) {
+      if(!picked.alreadypicked)
         {
-          if(!picked.alreadypicked)
-          {
-            picked.player = turn;
+          picked.player = turn;
+          if(turn == 1)
             picked.col = color(255,0,0);
-            picked.alreadypicked = !picked.alreadypicked;
+          else if(turn == 2)
+            picked.col = color(0,0,255);
+          picked.alreadypicked = !picked.alreadypicked;
+          if(turn == 1)
+          {
             turn = 2;
             turn = rubiks.win(1);
-            if(turn == 3)
-            {
-              p1win = p1win+1;
-            }
           }
-        }
-        else if (turn == 2)
-        {
-          if(!picked.alreadypicked)
+          else if (turn == 2)
           {
-            picked.player = turn;
-            picked.col = color(0,0,255);
-            picked.alreadypicked = !picked.alreadypicked;
             turn = 1;
             turn = rubiks.win(2);
-            if(turn ==4)
-            {
-              p2win=p2win+1;
-            }
+          }
+          if(turn == 3)
+          {
+            p1win = p1win+1;
+          }
+          else if (turn == 4)
+          {
+            p2win=p2win+1;
           }
         }
-      }
+    }
   }
   if(mouseButton == LEFT)
   {
-    if(mouseX > width-60 && mouseY < 30)
-    {
-        rubiks = new bigcube();
-    }
+    b.reset(rubiks,mouseX,mouseY);
   }
 }
+
 void mouseMoved()
 {
-  textc = 100;
   for(Cube t : rubiks.cubes)
   {
     t.highlight = 0;
@@ -139,17 +140,4 @@ void mouseMoved()
   {
     picked.highlight = 200;
   }
-  if(mouseX > width-60 && mouseY < 30)
-  {
-    textc  = 255;
-  }
-}
-
-void reset()
-{
-  fill(0);
-  rect(width-60,10,50,30);
-  fill(textc);
-  text("reset",width-60,30);
-
 }
--- build/source/TicTacToe.java
+++ build/source/TicTacToe.java
@@ -23,38 +23,44 @@ public class TicTacToe extends PApplet {
 
 
 
-bigcube rubiks;
 
+bigcube rubiks;
+button b;
 PGraphicsOpenGL pg;
 PeasyCam pcam;
-int turn = 1;
-int time;
-int p1win = 0;
-int p2win = 0;
-int textc = 100;
-public void setup() {
+int turn = 1, time,p1win = 0, p2win =0, texts = 100;
+
+public void setup()
+{
   
   cursor(CROSS);
   pg = (PGraphicsOpenGL)createGraphics(width, height, OPENGL);
   pcam = new PeasyCam(this, 150);
   rubiks = new bigcube();
+  b = new button(width-60, 10, 50, 30, color(255), width-60, 30, "reset");
 }
-
-public void draw() {
+public void t(int c, String s,int w,int h)
+{
+  textSize(20);
+  fill(c);
+  text(s,w,h);
+}
+public void draw()
+{
   background(0,0,0);
-  pointLight(255,255,255,pcam.getPosition()[0],pcam.getPosition()[1],pcam.getPosition()[2]);
+  pointLight(255, 255, 255, pcam.getPosition()[0], pcam.getPosition()[1], pcam.getPosition()[2]);
   rubiks.display();
   pcam.beginHUD();
+  b.draw(mouseX,mouseY);
   textSize(20);
-  reset();
-  fill(255, 0, 0);
-  text("Player1 : "+p1win,0,height-10);
-  fill(0,0,255);
-  text("Player2 : "+p2win,width-110,height-10);
   if(turn == 1 || turn == 2)
-    time = (millis()-rubiks.start)/1000;
-  fill(255, 255, 255);
-  text("Time :"+time, width/30, 30);
+  {
+    time = (millis() - rubiks.start)/1000;
+  }
+  t(color(255,0,0),"Player1 : " + p1win ,0,height-10);
+  t(color(0,0,255),"Player2 : " + p2win,width-110 , height-10);
+  t(color(255,255,255),"Time :"+time, width/30, 30);
+
   if(turn == 1)
   {
     fill(255, 0, 0);
@@ -75,13 +81,14 @@ public void draw() {
     fill(0,0,255);
     text("Player 2 wins" , width/2 - width/10, 30);
   }
-
   pcam.endHUD();
+
   }
 
 
 public Cube pickCube(int x, int y) {
   Cube cube = null;
+
   pg.projection.set(((PGraphicsOpenGL)g).projection);
   pg.camera.set(((PGraphicsOpenGL)g).camera);
   pg.beginDraw();
@@ -92,6 +99,7 @@ public Cube pickCube(int x, int y) {
   for (int i = 0; i < rubiks.cubes.length; i++)
     rubiks.cubes[i].displayForPicker();
   int c = pg.get(x, y);
+
   pg.endDraw();
   for (int i = 0; i < rubiks.cubes.length; i++) {
     if (rubiks.cubes[i].pickCol == c) {
@@ -102,55 +110,48 @@ public Cube pickCube(int x, int y) {
   return cube;
 }
 
-
 public void mouseClicked  () {
   if (mouseButton == RIGHT)
   {
     Cube picked = pickCube(mouseX, mouseY);
-      if (picked != null) {
-        if(turn == 1)
+    if (picked != null) {
+      if(!picked.alreadypicked)
         {
-          if(!picked.alreadypicked)
-          {
-            picked.player = turn;
+          picked.player = turn;
+          if(turn == 1)
             picked.col = color(255,0,0);
-            picked.alreadypicked = !picked.alreadypicked;
+          else if(turn == 2)
+            picked.col = color(0,0,255);
+          picked.alreadypicked = !picked.alreadypicked;
+          if(turn == 1)
+          {
             turn = 2;
             turn = rubiks.win(1);
-            if(turn == 3)
-            {
-              p1win = p1win+1;
-            }
           }
-        }
-        else if (turn == 2)
-        {
-          if(!picked.alreadypicked)
+          else if (turn == 2)
           {
-            picked.player = turn;
-            picked.col = color(0,0,255);
-            picked.alreadypicked = !picked.alreadypicked;
             turn = 1;
             turn = rubiks.win(2);
-            if(turn ==4)
-            {
-              p2win=p2win+1;
-            }
+          }
+          if(turn == 3)
+          {
+            p1win = p1win+1;
+          }
+          else if (turn == 4)
+          {
+            p2win=p2win+1;
           }
         }
-      }
+    }
   }
   if(mouseButton == LEFT)
   {
-    if(mouseX > width-60 && mouseY < 30)
-    {
-        rubiks = new bigcube();
-    }
+    b.reset(rubiks,mouseX,mouseY);
   }
 }
+
 public void mouseMoved()
 {
-  textc = 100;
   for(Cube t : rubiks.cubes)
   {
     t.highlight = 0;
@@ -160,19 +161,6 @@ public void mouseMoved()
   {
     picked.highlight = 200;
   }
-  if(mouseX > width-60 && mouseY < 30)
-  {
-    textc  = 255;
-  }
-}
-
-public void reset()
-{
-  fill(0);
-  rect(width-60,10,50,30);
-  fill(textc);
-  text("reset",width-60,30);
-
 }
 public class bigcube
 {
@@ -275,6 +263,50 @@ public class bigcube
       }
       return turn == 1 ? 2:1;
     }
+}
+public class button
+{
+  int rx,ry,rw,rh,tx,ty;
+  int c;
+  String s;
+  int texts = 100;
+  public button(int rx, int ry, int rw, int rh,int c,int tx,int ty,String s)
+  {
+    this.rx = rx;
+    this.ry = ry;
+    this.rw = rw;
+    this.rh = rh;
+    this.tx = tx;
+    this.ty = ty;
+    this.s = s;
+    this.c = c;
+  }
+  public void draw(int x ,int y)
+  {
+    textSize(20);
+    this.texts = highlight(x,y);
+    fill(0);
+    rect(this.rx, this.ry, this.rw, this.rh);
+    fill(texts);
+    text(this.s,tx,ty);
+  }
+  public int highlight(int x,int y)
+  {
+    if (this.rx < x && (this.rx + this.rw) > x && this.ry < y && (this.ry + this.rh) > y)
+    {
+      return 255;
+    }
+    return 100;
+  }
+  public void reset(bigcube c, int x, int y)
+  {
+    if (this.rx < x && (this.rx + this.rw) > x && this.ry < y && (this.ry + this.rh) > y)
+    {
+      rubiks = new bigcube();
+      time = 0;
+      turn = 1;
+    }
+  }
 }
 public class Cube
 {
